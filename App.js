781a4ff79b40d67128573872f3afa6bd303a8015
Justin Zhao <justin@justinzhao.net>
2022-05-29 15:37:01 -0700

    working links

--- App.js
+++ App.js
@@ -1,29 +1,34 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
-
-import { Provider } from "react-redux";
+import Button from "@mui/material/Button";
 
 import Menu from "./components/Menu";
 import Content from "./components/Content";
 
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./style.css";
+import setImageData from "./components/ImageData/ImageData";
 
 export default function App() {
+  let [images, setImages] = useState(setImageData("favorites"));
+  const updateImages = (img) => {
+    setImages(setImageData(img));
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <Container maxWidth="xlg">
         <Grid container spacing={2}>
           <Grid item xs={12} sm={3} md={2}>
-            <Box sx={{ bgcolor: "#cfe8fc" }}>
-              <Menu />
+            <Box sx={{}}>
+              <Menu images={images} updateImages={updateImages} />
             </Box>
           </Grid>
           <Grid item xs={12} sm={9} md={10}>
-            <Box sx={{ bgcolor: "#123123", height: "100vh" }}>
-              <Content />
+            <Box sx={{ height: "100vh" }}>
+              <Content itemData={images} />
             </Box>
           </Grid>
         </Grid>
--- components/Menu.js
+++ components/Menu.js
@@ -1,28 +1,57 @@
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
 import { Typography } from "@mui/material";
 
-export default function Menu() {
+export default function Menu({ updateImages }) {
+  const handleClick = (img) => {
+    updateImages(img);
+  };
   return (
     <Container>
       <Box sx={{ pt: 10, pb: 5 }}>
-        <Link href="#">
+        <Link>
           <Typography variant="h5">Home</Typography>
         </Link>
-        <Link href="#">
+        <Link
+          href="#"
+          onClick={() => {
+            updateImages("favorites");
+          }}
+        >
           <Typography variant="h5">Favorites</Typography>
         </Link>
-        <Link href="#">
+        <Link
+          href="#"
+          onClick={() => {
+            updateImages("film");
+          }}
+        >
           <Typography variant="h5">Film</Typography>
         </Link>
-        <Link href="#">
+        <Link
+          href="#"
+          onClick={() => {
+            updateImages("nyc");
+          }}
+        >
           <Typography variant="h5">NYC</Typography>
         </Link>
-        <Link href="#">
+        <Link
+          href="#"
+          onClick={() => {
+            updateImages("la");
+          }}
+        >
           <Typography variant="h5">LA/OC</Typography>
         </Link>
-        <Link href="#">
+        <Link
+          href="#"
+          onClick={() => {
+            updateImages("friends");
+          }}
+        >
           <Typography variant="h5">Friends</Typography>
         </Link>
       </Box>
